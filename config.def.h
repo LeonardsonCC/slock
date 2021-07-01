e48e42e5c67a5f1bb79adbf80fa447435343240a
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-07-01 10:17:51 -0300

    Opacity and using config.def

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "leonardson";
+static const char *group = "leonardson";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
@@ -9,7 +9,7 @@ static const char *colorname[NUMCOLS] = {
 };
 
 /* lock screen opacity */
-static const float alpha = 1;
+static const float alpha = 0.9;
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
